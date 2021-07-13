b9613b1c09db148536a778f326174d66c0caa02f
Carlos Alfredo Villegas Alvarez <cvillegas2@hotmail.es>
2021-07-13 01:18:48 -0500

    Se agrega clase de configuracion

---
+++ CountrysRestApi/Helpers/Configuration.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CountrysRestApi.Helpers
+{
+    public class Configuration
+    {
+        public static int SLEEP_MILLISECONDS_SERVICE = 1500;
+
+        public static int DefaultPageNumber = 1;
+
+        public static int DefaultPageSize = 50;
+    }
+}
