6232b0504677dbae0ec1971b5b0a3b1f83ef79ce
Carlos Alfredo Villegas Alvarez <cvillegas2@hotmail.es>
2021-07-13 02:26:34 -0500

    Se agrega servicio de login y registro

--- CountrysRestApi/Helpers/Configuration.cs
+++ CountrysRestApi/Helpers/Configuration.cs
@@ -7,6 +7,8 @@ namespace CountrysRestApi.Helpers
 {
     public class Configuration
     {
+        public static string CONNECTION_BD_MYSQL_LOCAL_HOST = "server=localhost;port=3306;user=root;password=;database=examen_countrys";
+
         public static int SLEEP_MILLISECONDS_SERVICE = 1500;
 
         public static int DefaultPageNumber = 1;
---
+++ ModelsCountrysRestApi/Response/ResponseLogin.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModelsCountrysRestApi.Response
+{
+    public class ResponseLogin
+    {
+        public string Token { get; set; }
+        public int ExpireDate { get; set; }
+    }
+}
