4c7fa7bb975c4b2a4c4cadcfd4217d6658b19aa9
Carlos Alfredo Villegas Alvarez <cvillegas2@hotmail.es>
2021-07-16 02:25:25 -0500

    se agrega documentacion de swagger

---
+++ CountrysRestApi/Helpers/SwaggerConfiguration.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CountrysRestApi.Helpers
+{
+    public class SwaggerConfiguration
+    {
+
+        /// <summary>
+        /// <para>Foo API v1</para>
+        /// </summary>
+        public const string EndpointDescription = "Examen API v1";
+
+        /// <summary>
+        /// <para>/swagger/v1/swagger.json</para>
+        /// </summary>
+        public const string EndpointUrl = "/swagger/v1/swagger.json";
+
+        /// <summary>
+        /// <para>Jorge Serrano</para>
+        /// </summary>
+        public const string ContactName = "Carlos Villegas";
+
+        /// <summary>
+        /// <para>http://google.com</para>
+        /// </summary>
+        public const string ContactUrl = "http://google.com";
+
+        /// <summary>
+        /// <para>v1</para>
+        /// </summary>
+        public const string DocNameV1 = "v1";
+
+        /// <summary>
+        /// <para>Foo API</para>
+        /// </summary>
+        public const string DocInfoTitle = "Examen API";
+
+        /// <summary>
+        /// <para>v1</para>
+        /// </summary>
+        public const string DocInfoVersion = "v1";
+
+        /// <summary>
+        /// <para>Foo Api - Sample Web API in ASP.NET Core 2</para>
+        /// </summary>
+        public const string DocInfoDescription = "Examen Api - Paises";
+
+    }
+}
